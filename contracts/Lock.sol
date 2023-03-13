f8fbc74bb4817b1d2430e232ca2a2d04c43dff1d
omkarg01 <omkargujja01@gmail.com>
2023-03-13 17:40:15 +0530

    variables declare

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,5 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
+contract RandomWinnerGame is VRFConsumerBase, Ownable {
+    //Chainlink variables
+    // The amount of LINK to send with the request
+    uint256 public fee;
 
+    // ID of public key against which randomness is generated
+    bytes32 public keyHash;
+
+    // Address of the players
+    address[] public players;
+
+    //Max number of players in one game
+    uint8 maxPlayers;
+
+    // Variable to indicate if the game has started or not
+    bool public gameStarted;
+
+    // the fees for entering the game
+    uint256 entryFee;
+
+    // current game id
+    uint256 public gameId;
+
+    // emitted when the game starts
+    event GameStarted(uint256 gameId, uint8 maxPlayers, uint256 entryFee);
+    // emitted when someone joins a game
+    event PlayerJoined(uint256 gameId, address player);
+    // emitted when the game ends
+    event GameEnded(uint256 gameId, address winner, bytes32 requestId);
+
+    
+}
